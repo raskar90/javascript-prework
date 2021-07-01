76be7adb99a8808401a60d0faedd0e55574ddd8c
Karolina Rasińska <rasinska.karolina@gmail.com>
2021-07-01 17:49:05 +0200

    Add else if 2 and 3

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
-function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+} else if(randomNumber == 2){
+  computerMove = 'papier';
+} else {
+  computerMove = 'nożyce';
 }
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
-
-let playerMove = `papier`;
-
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+printMessage('Mój ruch to: ' + computerMove);
