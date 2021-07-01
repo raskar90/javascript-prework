3b666933378adfd042b5ca6257163186708c2626
Karolina Rasińska <rasinska.karolina@gmail.com>
2021-07-01 18:56:39 +0200

    Game results

--- js/script.js
+++ js/script.js
@@ -29,3 +29,25 @@ if(playerInput == '1'){
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz!');
+} else if( computerMove == 'kamień' && playerMove == 'kamień') {
+  printMessage('Remis!');
+} else if( computerMove == 'papier' && playerMove == 'kamień') {
+  printMessage('Niestety przegrywasz!');
+} else if( computerMove == 'nożyce' && playerMove == 'kamień') {
+  printMessage( 'Ty wygrywasz!');
+} else if( computerMove == 'nożyce' && playerMove == 'nożyce') {
+  printMessage( 'Remis!');
+} else if( computerMove =='kamień' && playerMove == 'nożyce') {
+  printMessage( 'Niestety przegrywasz!');
+} else if( computerMove == 'papier' && playerMove =='nożyce') {
+  printMessage( 'Ty wygrywasz!');
+} else if( computerMove =='papier' && playerMove == 'papier') {
+  printMessage( 'Remis!');
+} else if( computerMove == 'nożyce' && playerMove =='papier') {
+  printMessage( 'Niestety przegrywasz');
+} else if( playerMove='nieznany ruch') {
+  printMessage( 'Wprowadź poprawną liczbę, tj. liczbę z przedziału 1, 2 lub 3');
+}
